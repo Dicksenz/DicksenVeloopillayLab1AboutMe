5f80118bb96f36198ab0c5f57941d8bb3d15afa6
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2022-09-08 19:47:05 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import Header from './components/Header';
+import SubHeader from './components/SubHeader';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <SubHeader />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+export default App;
